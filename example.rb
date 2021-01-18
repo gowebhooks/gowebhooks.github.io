0b1bececd5585c35d5a5286185fb53fb171bf76f
Jacqueline Xu <jxu703@gmail.com>
2021-01-18 15:41:51 -0800

    Copy changes

--- example.rb
+++ example.rb
@@ -4,9 +4,11 @@
 Gowebhook.api_key = 'gw_dsfKJSD93rKS39sfhdsf9334bxxw'
 
 Gowebhook.send(
-  user_id: 'bob_1234',
-  contact_email: 'bob@bob.com',
-  endpoints: ['https://bob.com/invoice_consumer', 'https://slack.com/bob/invoices'],
+  customer_id: 'bob_1234',
   event_name: 'invoice.paid',
   event_payload: {invoice: {status: 'paid'}}
 )
+
+
+
+
